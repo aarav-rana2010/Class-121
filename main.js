b59c611b59d431417d839dabc3e5165c7475636d
aarav-rana2010 <78865218+aarav-rana2010@users.noreply.github.com>
2022-06-05 16:00:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier=ml5.imageClassifier("MobileNet",modelLoaded)
+}
+function draw() {
+  image(video,0,0,300,300);
+  classifier.classify(video,gotResult);
+}
+function modelLoaded() {
+  console.log("modelloaded");
+}
+var previous_result="";
+function gotResult(error,results) {
+if (error) {
+  console.error(error);
+}
+else {
+  if ((results[0].confidence>0.5)&& (previous_result!=results[0].label)){
+    console.log(results);
+    previous_result=reults[0].label;
+    var synth=window.speechSynthesis;
+    speak_data="object detected is " + results[0].label;
+    var utterthis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_objet_acuracy").innerHTML=results[0].confidence.toFixed(3);
+  }
+}
+}
+
+
